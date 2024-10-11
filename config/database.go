a542cda16350378480787dee7b75d5ede2affcad
Efe Kaya <me@efekaya.co>
2024-10-10 23:15:36 -0600

    Refactor database configuration and connection code

---
+++ config/database.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		host, user, password, dbname, port)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database: " + err.Error())
+	}
+
+	fmt.Println("Database connection successfully established")
+}
--- main.go
+++ main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
+	"justtodothings/config"
 	"log"
 
-	"github.com/rockswe/justtodothings/db" // Update the module name as needed
-
-	"github.com/gin-gonic/gin" // Web framework for handling HTTP requests
-	"github.com/joho/godotenv" // Library for loading environment variables
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
-
 	if err != nil {
-		log.Fatalf("Failed to load .env file")
+		log.Fatalf("Error loading .env file")
 	}
 
-	db.ConnectDatabase()
-
-	router := gin.Default()
-
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Server is running",
-		})
-	})
-
-	// Start the server on port 8080
-	err = router.Run(":8080")
-	if err != nil {
-		log.Fatalf("Error starting the server: %v", err)
-	}
+	config.ConnectDatabase()
 
 }
