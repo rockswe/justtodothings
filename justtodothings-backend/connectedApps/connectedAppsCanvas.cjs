204d1517151df8f23445736073021495c054add7
Efe Kaya <sithban2@gmail.com>
2025-05-09 16:08:30 -0600

    DB connection fix

--- justtodothings-backend/connectedApps/connectedAppsCanvas.cjs
+++ justtodothings-backend/connectedApps/connectedAppsCanvas.cjs
@@ -7,9 +7,13 @@ const crypto = require("crypto");
 
 const s3 = new AWS.S3();
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,     
+  port: process.env.DB_PORT || 5432,   
+  user: process.env.DB_USER,           
+  password: process.env.DB_PASS,     
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true }
 });
-
 // ---------- Helper Functions ----------
 
 // Attach a stable source_id to each Canvas item.
